a812b09cf15c9624dab87808eadc7399c04cc839
Shen Jun Wu <shenfive@gmail.com>
2022-08-03 17:13:41 +0800

    Initial Commit

---
+++ CS193P-1/CS193P_1App.swift
@@ -0,0 +1,17 @@
+//
+//  CS193P_1App.swift
+//  CS193P-1
+//
+//  Created by 申潤五 on 2022/8/3.
+//
+
+import SwiftUI
+
+@main
+struct CS193P_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
